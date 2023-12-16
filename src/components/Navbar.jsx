f1ee171bbc29274971be60fe4f2a79a64004bcd6
Nurik <102224735+nurikmolodes@users.noreply.github.com>
2023-12-16 15:12:52 +0600

    that is it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/components/Navbar.scss";
 import { useNavigate } from "react-router-dom";
 import upperVector from "../assets/upperVector.svg";
 
 const Navbar = () => {
   const [activeButton, setActiveButton] = useState("Home");
+  useEffect(() => {
+    const activeRoute = localStorage.getItem("activeRoute");
+    if (activeRoute) {
+      setActiveButton(activeRoute);
+    }
+  }, []);
   const navigate = useNavigate();
+  const handleClick = (active, route) => {
+    setActiveButton(active);
+    localStorage.setItem("activeRoute", active);
+    navigate(`${route}`);
+  };
   return (
     <div className="navbar">
       <img src={upperVector} className="upperVector" />
@@ -14,16 +25,14 @@ const Navbar = () => {
         <button
           className={activeButton === "Home" && "active"}
           onClick={() => {
-            setActiveButton("Home");
-            navigate("/");
+            handleClick("Home", "/");
           }}>
           Home
         </button>
         <button
           className={activeButton === "About" && "active"}
           onClick={() => {
-            setActiveButton("About");
-            navigate("/about");
+            handleClick("About", "/about");
           }}>
           About
         </button>
